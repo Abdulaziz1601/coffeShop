60737137b800a1c20bedd5c299ac69e094df5a19
Abdulaziz <abdulazizfaactory@gmail.com>
2021-12-24 18:04:58 +0500

    page 2 is done

--- src/components/cardFilter/cardFilter.js
+++ src/components/cardFilter/cardFilter.js
@@ -1,36 +1,80 @@
 
+import { Component } from 'react/cjs/react.production.min';
 import CardItem from '../cardItem/cardItem';
 import './cardFilter.scss';
 
 
-const CardFilter = ({data}) => {
-	const cards = data.map((item) => (
-		<CardItem {...item} />
-	));
-	return (
-        <section className='cardFilter'>
-            {/* <hr className='card__line'/> */}
-        <div className='cardFilter__wrapper'>
-			<form className='cardFilter__form'>
-				<span>Lookiing for</span>
-				<input className='cardFilter__input' type="text" placeholder='start typing here...' />
-			</form>
-			<div className='cardFilter__filter'>
-				<span>Or filter</span>
-				<div className='cardFilter__btn-wrapper'>
-					<button className='cardFilter__btn'>Brazil</button>
-					<button className='cardFilter__btn'>Kenya</button>
-					<button className='cardFilter__btn'>Columbia</button>
+class CardFilter extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			term: ''
+		};
+
+		this.buttons = [
+			{place: 'Brazil'},
+			{place: 'Kenya'},
+			{place: 'Columbia'},
+		]
+
+	}
+
+	onUpdateSearch = (e) => {
+		const term = e.target.value;
+		console.log(term);
+		this.setState({
+			term
+		});
+		this.props.onUpdateSearch(term);
+	}
+	
+	render() {
+		const {data} = this.props;
+		const cards = data.map((item, i) => (
+			<CardItem key={i} {...item} />
+		));
+		const buttons = this.buttons.map(({place}) => {
+			const active = this.props.filter === place;
+			const clazz = active ? 'cardFilter__btn_active': null;
+			return <button type='button' 
+						className={`cardFilter__btn ${clazz}`}
+						key={place} 
+						onClick={() => this.props.onButtonsFilter(place)}>
+						{place}
+					</button>
+		})
+		return (
+
+			<section className='cardFilter'>
+				{/* <hr className='card__line'/> */}
+			<div className='cardFilter__wrapper'>
+				<form className='cardFilter__form'>
+					<span>Lookiing for</span>
+					<input 
+						className='cardFilter__input'
+						type="text"
+						placeholder='start typing here...'  
+						onChange={this.onUpdateSearch}/>
+				</form>
+				<div className='cardFilter__filter'>
+					<span>Or filter</span>
+					<div className='cardFilter__btn-wrapper'>
+						{/* <button className='cardFilter__btn cardFilter__btn_active'>Brazil</button>
+						<button className='cardFilter__btn'>Kenya</button>
+						<button className='cardFilter__btn'>Columbia</button> */}
+						{buttons}
+					</div>
 				</div>
 			</div>
-        </div>
-
-        <div className='cardFilter__card-wrapper'>
-			{cards}
-		</div>
-
-        </section> 
-	)
+	
+			<div className='cardFilter__card-wrapper'>
+				{cards}
+			</div>
+	
+			</section> 
+		)
+	}
 };
 
 export default CardFilter;
--- src/components/page_2/page_2.js
+++ src/components/page_2/page_2.js
@@ -1,3 +1,4 @@
+import { Component } from "react/cjs/react.production.min";
 import AboutBeans from "../aboutBeans/aboutBeans";
 import Aboutus from "../aboutus/aboutus";
 import Best from "../best/best";
@@ -8,14 +9,58 @@ import Header from "../header/header";
 import Poster from "../poster/poster";
 import './page_2.scss';
 
-const Page_2 = ({data}) => {
-	return (
-        <div>   
-            <Header heading={"Our Coffee"} />
-            <AboutBeans />
-            <CardFilter data={data} />            
-        </div> 
-	)
+class Page_2 extends Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            term: '',
+            filter: 'Brazil'
+        }
+    }
+
+    
+
+    onUpdateSearch = (term) => {
+        this.setState({term});
+    }
+
+    searchCard = (items, term) => {
+        if(term.length === 0) {
+            return items;
+        }
+        return items.filter(item => (
+            item.name.indexOf(term) > -1
+        ))
+    }
+
+
+    onButtonsFilter = (filter) => {
+        this.setState({filter});
+    }
+
+    filterCards = (data, filter) => {
+        return data.filter(item => item.place === filter)
+    }
+
+
+    render() {
+        const {filter, term} = this.state;
+        const {data} = this.props;
+        const searchedData = this.searchCard(data, term);
+        const visibleData = this.filterCards(searchedData, filter);
+        return (
+            <div>   
+                <Header heading={"Our Coffee"} />
+                <AboutBeans />
+                <CardFilter data={visibleData}
+                            onUpdateSearch={this.onUpdateSearch}
+                            onButtonsFilter={this.onButtonsFilter}
+                            filter={filter}/> 
+                <Footer />           
+            </div> 
+        )
+    }
 };
 
 export default Page_2;
